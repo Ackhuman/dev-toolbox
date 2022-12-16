f96bf608d1558c7fa44cc98e2d19e4aef9d998a6
Ackhuman <ryan.acumen@gmail.com>
2022-12-16 15:06:40 -0500

    Add C# implementation of oxford

---
+++ CollectionFunctions.cs
@@ -0,0 +1,25 @@
+namespace DevToolbox.Helpers 
+{
+  public static class CollectionFunctions
+  {
+    /// <summary>
+    /// Joins items into a natural language list, e.g. "A, B, and C"
+    /// </summary>
+    /// <param name="items"></param>
+    /// <param name="listSeparator"></param>
+    /// <param name="listTerminator"></param>
+    /// <returns></returns>
+    public static string Oxford(this IEnumerable<string> items, string listSeparator = ",", string listTerminator = "and")
+    {
+        //special case, e.g. "A and B", which should not have commas
+        if (items.Count() == 2)
+        {
+            return string.Join($" {listTerminator} ", items);
+        }
+        var lastItemIndex = items.Count() - 1;
+        var commaSet = items.Take(lastItemIndex).Select(i => $"{i}{listSeparator}");
+        var lastItem = items.Skip(lastItemIndex).Select(i => $"{listTerminator} {i}");
+        return string.Join(" ", commaSet.Concat(lastItem));
+    }
+  }
+}
